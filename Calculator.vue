78d9f78e672830cb4e0f8f4f70b05d34c8e7b577
MariaChowdhury <maria.chowdhury@gmail.com>
2022-04-07 16:27:48 -0700

    initial layout

---
+++ Calculator.vue
@@ -0,0 +1,59 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="calculator">
+     <div > Principal (P): $</div>
+     <input v-model="text">
+     <div> Annual Rate (R): %</div>
+     <input v-model="text">
+     <div> Compound (n):</div>
+     <input v-model="text">
+     <div> Time (t in years):</div>
+     <input v-model="text">
+     <button @click="calculate()" class="display" > Calculate</button>
+     <div></div>
+     <div>Answer:{{current}}</div>
+     <div> </div>
+     
+  </div>
+  
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      current: '123'
+    }
+    
+  },
+  methods:{
+    clear() {
+       this.current='';
+    },
+    calculate(){
+      
+    }
+  }
+}
+  
+</script>
+
+<style scoped>
+.calculator{
+  width: 400px;
+  display: grid;
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  grid-template-columns: repeat(2,150px);
+  grid-template-rows: (1,50px);
+}
+.display{
+  color: rgb(7, 7, 7);
+  grid-column: 2;
+  margin: 5px;
+}
+
+</style>
